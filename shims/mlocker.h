6407557d5636d9e2544bc22660fb2c20fb098d1c
John D <az0006t@protonmail.com>
2026-06-11 19:23:48 -0400

    Update mlocker.h

--- shims/mlocker.h
+++ shims/mlocker.h
@@ -1,21 +1,44 @@
 #pragma once
 
 #include <stddef.h>
+#include <string.h>
+#include <utility>
 
 namespace epee
 {
   class mlocker
   {
   public:
-    mlocker(const void *addr, size_t len)
-    {
-      (void)addr;
-      (void)len;
-    }
-
+    mlocker(const void *addr, size_t len) { (void)addr; (void)len; }
     ~mlocker() {}
 
     mlocker(const mlocker&) = delete;
     mlocker& operator=(const mlocker&) = delete;
+  };
+
+  template<typename T>
+  struct mlocked : public T
+  {
+    mlocked() : T() {}
+
+    mlocked(const T& other) : T(other) {}
+
+    mlocked(const mlocked& other) : T(static_cast<const T&>(other)) {}
+
+    mlocked& operator=(const mlocked& other)
+    {
+      if (this != &other) {
+        static_cast<T&>(*this) = static_cast<const T&>(other);
+      }
+      return *this;
+    }
+
+    mlocked& operator=(const T& other)
+    {
+      static_cast<T&>(*this) = other;
+      return *this;
+    }
+
+    ~mlocked() {}
   };
 }
